a32a12acf13aab37f06a601a55f4a1554135d3b3
aparna-jha-23 <121332698+aparna-jha-23@users.noreply.github.com>
2023-04-06 07:45:35 +0530

    Add files via upload

--- src/components/Navbarnew.js
+++ src/components/Navbarnew.js
@@ -21,18 +21,18 @@ const NavbarNew = () => {
   return (
 
 <>
-      <div className="navbar">
-        <div className="group-parent1">
+      <div className="navbarnew">
+        <div className="group-parent1new">
           
           {/* <div className="home" onClick={ () => {navigate(-1)}}>HOME</div>
           <div className="report">REPORT</div> */}
 
           {/* <div className="about-us2">{`ABOUT US `}</div> */}
            
-          <button className="backbutton" >BACK</button>
+          <button className="backbuttonnew" >BACK</button>
         </div>
         <img
-          className="find-your-balance-1-1"
+          className="find-your-balance-1-1new"
           alt=""
           src="../find-your-balance1-1@2x.png"
         />
